ff6ea2e315deb4a496aaa934995aa8dcb378ed5e
kogetsu7 <kogetsu0728@gmail.com>
2024-12-16 16:56:22 +0900

    add: real

--- template/types.hpp
+++ template/types.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <complex>
 #include <cstddef>
 #include <cstdint>
 #include <string>
@@ -13,63 +14,64 @@ using iinf_t = boost::multiprecision::cpp_int;
 #endif
 
 namespace kgt {
+using i8_t = std::int_least8_t;
+using i16_t = std::int_least16_t;
+using i32_t = std::int_least32_t;
+using i64_t = std::int_least64_t;
+using i128_t = __int128_t;
+
+using u8_t = std::uint_least8_t;
+using u16_t = std::uint_least16_t;
+using u32_t = std::uint_least32_t;
+using u64_t = std::uint_least64_t;
+using u128_t = __uint128_t;
+
+using size_t = std::size_t;
+
+using real_t = long double;
+using complex_t = std::complex<real_t>;
+
 template <class T>
 using is_signed_int = std::bool_constant<(std::is_integral<T>::value &&
                                           std::is_signed<T>::value) ||
-                                         std::is_same<T, __int128_t>::value>;
+                                         std::is_same<T, i128_t>::value>;
 
 template <class T>
 using is_unsigned_int = std::bool_constant<(std::is_integral<T>::value &&
                                             std::is_unsigned<T>::value) ||
-                                           std::is_same<T, __uint128_t>::value>;
+                                           std::is_same<T, u128_t>::value>;
 
 template <class T>
 using is_int =
     std::bool_constant<is_signed_int<T>::value || is_unsigned_int<T>::value>;
-
-using size_t = std::size_t;
 
 template <size_t D>
 using signed_int_least = std::enable_if<
     D <= 128,
     typename std::conditional<
         D <= 8,
-        std::int_least8_t,
+        i8_t,
         typename std::conditional<
             D <= 16,
-            std::int_least16_t,
+            i16_t,
             typename std::conditional<
                 D <= 32,
-                std::int_least32_t,
-                typename std::
-                    conditional<D <= 64, std::int_least64_t, __int128_t>::
-                        type>::type>::type>::type>;
+                i32_t,
+                typename std::conditional<D <= 64, i64_t, i128_t>::type>::
+                type>::type>::type>;
 
 template <size_t D>
 using unsigned_int_least = std::enable_if<
     D <= 128,
     typename std::conditional<
         D <= 8,
-        std::uint_least8_t,
+        u8_t,
         typename std::conditional<
             D <= 16,
-            std::uint_least16_t,
+            u16_t,
             typename std::conditional<
                 D <= 32,
-                std::uint_least32_t,
-                typename std::
-                    conditional<D <= 64, std::uint_least64_t, __uint128_t>::
-                        type>::type>::type>::type>;
-
-using i8_t = typename signed_int_least<8>::type;
-using i16_t = typename signed_int_least<16>::type;
-using i32_t = typename signed_int_least<32>::type;
-using i64_t = typename signed_int_least<64>::type;
-using i128_t = typename signed_int_least<128>::type;
-
-using u8_t = typename unsigned_int_least<8>::type;
-using u16_t = typename unsigned_int_least<16>::type;
-using u32_t = typename unsigned_int_least<32>::type;
-using u64_t = typename unsigned_int_least<64>::type;
-using u128_t = typename unsigned_int_least<128>::type;
+                u32_t,
+                typename std::conditional<D <= 64, u64_t, u128_t>::type>::
+                type>::type>::type>;
 };  // namespace kgt
